f841e0c30c9e67651546e02bbe5f36881fde8744
Haprons <yadunandpradeep90@gmail.com>
2025-07-04 10:35:35 +0530

    Time: 2 ms (75.09%) | Memory: 7.7 MB (88.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.c
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -1,6 +1,11 @@
-
-
 int fib(int n){
     if(n == 0|| n == 1 ) return n;
-    return fib(n-1) + fib(n-2);
+    int a = 0,b= 1;
+    int sum;
+    for(int i=2;i< n + 1;i++ ){
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 }
