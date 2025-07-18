858c035cb624e4652440800559bbe399f2e2f4cf
Haprons <yadunandpradeep90@gmail.com>
2025-07-18 06:21:35 +0530

    Time: 219 ms (100.00%) | Memory: 44.1 MB (95.91%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
@@ -1,37 +1,36 @@
-import heapq
-
 class Solution:
-    def minimumDifference(self, nums: list[int]) -> int:
-        m = len(nums)
-        n = m // 3
-        left = [0] * m
-        maxheap = []
-        prefSum = 0
-        for i in range(n):
-            prefSum += nums[i]
-            heapq.heappush(maxheap, -nums[i])
-        left[n-1] = prefSum
-        for i in range(n, m):
-            heapq.heappush(maxheap, -nums[i])
-            val = -heapq.heappop(maxheap)
-            prefSum += nums[i] - val
-            left[i] = prefSum
+    def minimumDifference(self, V: List[int]) -> int:
+        n = len(V)//3
 
-        right = [0] * m
-        minheap = []
-        suffSum = 0
-        for i in range(m-1, m-n-1, -1):
-            suffSum += nums[i]
-            heapq.heappush(minheap, nums[i])
-        right[m-n] = suffSum
-        for i in range(m-n-1, -1, -1):
-            heapq.heappush(minheap, nums[i])
-            val = heapq.heappop(minheap)
-            suffSum += nums[i] - val
-            right[i] = suffSum
+        # for each closed range [0,n], ... [0,2n-1] find min. n elements
+        ## store their sum
+        H = [-v for v in V[:n]]
+        heapify(H)
+        P = [-sum(H)]                # prefix sum
+        for v in V[n: 2*n]:
+            p = P[-1]
+            if -v > H[0]:
+                p -= -heappop(H)
+                heappush(H, -v)
+                p += v
+            P.append(p)
+        #print(P)
 
-        diff = float('inf')
-        for i in range(n-1, 2*n):
-            diff = min(diff, left[i] - right[i+1])
-        return diff
-        
+        # for each range [n,3n], ... [2n,3n] find max. n elements
+        ## store their sum
+        H = [v for v in V[2*n:]]
+        heapify(H)
+        S = [sum(H)]                # suffix sum
+        for v in V[2*n-1: n-1: -1]:
+            s = S[-1]
+            if v > H[0]:
+                s -= heappop(H)
+                heappush(H, v)
+                s += v
+            S.append(s)
+        S = S[::-1]
+        #print(S)
+
+        # cost is sum-min(N[0:n-1]) + sum-max(N[n:3n-1]) - closed ranges
+        # here P starts at n-1 and S[::-1] ends at n - hence zip directly
+        return min(p-s for p,s in zip(P,S))
