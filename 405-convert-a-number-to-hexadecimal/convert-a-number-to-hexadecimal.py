87c983b8f876f3adedf41d2878612859fa90ecdd
Haprons <yadunandpradeep90@gmail.com>
2025-07-18 06:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (36.26%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -3,6 +3,10 @@ class Solution:
         if num == 0:
             return "0"
         if num < 0:
-            num += 2 ** 32 
-        hex_con = hex(num)[2:]
-        return hex_con
+            num += 2**32
+        h = "0123456789abcdef"
+        r = []
+        while num:
+            r.append(h[num & 15])
+            num //= 16
+        return "".join(r[::-1])
