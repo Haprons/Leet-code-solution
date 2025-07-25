93a975710252696f164183cbe7a37f9efe142c1b
Haprons <yadunandpradeep90@gmail.com>
2025-07-25 19:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (99.41%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        h_index = 0
+        for i in range(len(citations)-1,-1,-1):
+            if citations[i] > h_index:
+                h_index += 1
+        return h_index
