5f377a84c1c944265793b2f75b36e23bf283f0f1
Haprons <yadunandpradeep90@gmail.com>
2025-07-25 19:20:59 +0530

    Time: 333 ms (29.57%) | Memory: 17.5 MB (98.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n==0 or n==1:
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
-        
+        return self.fib(n-1) + self.fib(n-2)
