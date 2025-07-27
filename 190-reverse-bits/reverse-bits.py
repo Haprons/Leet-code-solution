100e3f5f93feb5cdba32be512b0eb87ecf108e44
Haprons <yadunandpradeep90@gmail.com>
2025-07-27 16:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.12%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,4 +1,11 @@
-
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for _ in range(32):
+            bit = n & 1
+            result = (result << 1) | bit
+            n >>= 1
+        return result
 import atexit
 atexit.register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
