3f12d0f25de6cb71205425fcd3cd98231011298a
Haprons <yadunandpradeep90@gmail.com>
2025-07-27 16:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.44%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -8,8 +8,3 @@ class Solution:
         return result
 import atexit
 atexit.register(lambda: open("display_runtime.txt", "w").write("0"))
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        bin_str = '{:032b}'.format(n)
-        reversed_bin = bin_str[::-1]
-        return int(reversed_bin, 2)
