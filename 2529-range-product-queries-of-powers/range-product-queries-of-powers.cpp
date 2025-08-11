b77bb4671485e45ec5cba234165675b688ff91e3
Haprons <yadunandpradeep90@gmail.com>
2025-08-11 06:41:10 +0530

    Time: 0 ms (100.00%) | Memory: 145.2 MB (79.26%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,32 +1,20 @@
-static constexpr int mod=1e9+7, B30=(1<<30)%mod;
 class Solution {
 public:
-    static int modPow(long long x, int exp){
-        if (exp==0) return 1;
-        long long y=(exp&1)?x:1;
-        return modPow(x*x%mod, exp>>1)*y%mod;
-    }
-    static int pow2mod(int exp){
-        if (exp<30) return 1<<exp;
-        auto [q, r]=div(exp, 30);
-        long long B=modPow(B30, q);
-        return B*(1<<r)%mod;
-    }
-    static vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        const int m=queries.size();
-        bitset<30> B(n);
-        vector<int> exp2;
-        for(int i=0; i<30; i++)
-            if (B[i]) exp2.push_back(i);
-
-        partial_sum(exp2.cbegin(), exp2.cend(), exp2.begin());
-
-        vector<int> ans(m);
-        for(int i=0; i<m; i++){
-            const int s=queries[i][0], e=queries[i][1];
-            const int exp=exp2[e]-((s==0)?0:exp2[s-1]);
-            ans[i]=pow2mod(exp);
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        constexpr int MOD = 1000000007;
+        vector prefix {0};
+        while (n) {
+            const int j = __builtin_ctz(n);
+            prefix.push_back(prefix.back() + j);
+            n -= 1 << j;
         }
-        return ans;
+        n = prefix.back();
+        vector ans {1}; ans.reserve(n);
+        for (int i = 1; i <= n; i++)
+            ans.push_back((ans.back() << 1) % MOD);
+        vector<int> ret; ret.reserve(queries.size());
+        for (const auto& q : queries)
+            ret.push_back(ans[prefix[q[1] + 1] - prefix[q[0]]]);
+        return ret;
     }
 };
