c5ceb42e3b2b598ce21e87a423e05f86674b0cc8
Haprons <yadunandpradeep90@gmail.com>
2025-08-12 05:44:25 +0530

    Time: 2557 ms (8.72%) | Memory: 681.4 MB (7.85%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,10 +1,14 @@
+from math import pow
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
-        p = [q**x for q in range(1,round(n**(1/x))+1)]
+        self.sq = pow(n, 1/x)+1
         @cache
-        def f(i,n):
-            if i<len(p) and n>=p[i]:
-                return (f(i+1,n)+f(i+1,n-p[i]))%(10**9+7)
-            return int(n==0)
-            
-        return f(0,n)
+        def bt(n, i):
+            p = int(n-pow(i, x))
+            if p < 0 or i > self.sq:
+                return 0
+            if p == 0:
+                return 1
+            return bt(p, i+1)+bt(n,i+1)
+        
+        return bt(n, 1)%(10**9+7)
