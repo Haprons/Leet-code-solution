674d068f56f7d91b1b1475c7d9da21af28dd2055
Haprons <yadunandpradeep90@gmail.com>
2025-08-12 05:48:12 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (28.69%) - LeetSync

--- 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.java
@@ -1,14 +1,26 @@
 class Solution {
     public int[] evenOddBit(int n) {
-        String s = Integer.toBinaryString(n);
-        int[] res = new int[2];       
-        // Traverse from right to left (LSB to MSB)
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(s.length() - 1 - i); // reverse index
-            if (ch == '1') {
-                res[i % 2]++; // i%2==0 => even bit, i%2==1 => odd bit
+        int arr[] = new int[2];
+
+        int even=0;
+        int odd=0;
+        int pos=0;
+        while(n!=0)
+        {
+            int a = (n&1);
+            if(a==1)
+            {
+                if(pos%2==0)
+                even++;
+                else
+                odd++;
+
             }
-        }        
-        return res;
+            pos++;
+            n>>=1;
+        }
+        arr[0]=even;
+        arr[1]=odd;
+        return arr;
     }
 }
