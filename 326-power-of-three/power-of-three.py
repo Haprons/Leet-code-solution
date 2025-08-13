734f1b2825f192c8eec34bab1d81f97911b4c343
Haprons <yadunandpradeep90@gmail.com>
2025-08-13 08:47:28 +0530

    Time: 20 ms (19.75%) | Memory: 17.8 MB (72.35%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,10 +1,5 @@
-from math import log
-class Solution(object):
-    def isPowerOfThree(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
         if n<=0: return False
         res = round(log(n,3))
         return 3**res == n
