c87c4027299d78c3d393c87f1957049fd0fc1b3e
Haprons <yadunandpradeep90@gmail.com>
2025-08-15 11:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (90.85%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<string> a, tmp = {"0","1"};
-        vector<int> res;
-        for(int i = 2; i <= n;i++){
-            a = tmp;
-            tmp.clear();
-            for(string s : a) tmp.push_back('0' + s);
-            reverse(a.begin(),a.end());
-            for(string s : a) tmp.push_back('1' + s);
+        int N=pow(2,n);
+        vector<int> gray(N);
+        for(int i=0;i<N;i++){
+            gray[i]=(i^(i>>1));
         }
-        for (string s : tmp) res.push_back(stoi(s,nullptr,2));
-        return res;
+
+        return gray;
+
     }
 };
