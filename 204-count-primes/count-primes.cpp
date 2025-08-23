705d8c186773a64ca53c70b2ab8498f02875fc51
YadunandPradeep <yadunandpradeep90@gmail.com>
2025-08-23 12:05:37 +0530

    Time: 138 ms (93.13%) | Memory: 132.5 MB (17.89%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n < 2) return 0;
+        vector<int>prime(n,1);
+        prime[0] = prime[1] = 0;
+        int count = 0;
+        for(int i = 2; i*i < n ; i++ ){
+            if(prime[i]){
+                for(int j = i*i; j < n; j += i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        for (int h = 0 ; h < n ; h++){
+            if(prime[h])
+                count++;
+        }
+        return count;
+    }
+};
