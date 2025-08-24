ea0b23bcf8dea79affa8344dfa30ac32cb2072f5
YadunandPradeep <yadunandpradeep90@gmail.com>
2025-08-24 15:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (80.51%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
     int rob1 = 0,rob2 = 0;
-
+    //[rob1,rob2,nums[i],nums[i+1],...]
     for(int i = 0 ; i < nums.size() ; i++)
     {
         int temp = max(nums[i]+rob1,rob2);
